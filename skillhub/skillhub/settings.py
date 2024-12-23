ac53034574d67676bdeb9236a3f8c11cca98fe59
Mariam Khokhiashvili <142008382+MariamKhoKh@users.noreply.github.com>
2024-12-23 21:07:32 +0400

    🧹

--- skillhub/skillhub/settings.py
+++ skillhub/skillhub/settings.py
@@ -155,8 +155,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = 'email'
+EMAIL_HOST_PASSWORD = 'app password'
 
 
 CELERY_BROKER_CONNECTION_RETRY_ON_STARTUP = True
